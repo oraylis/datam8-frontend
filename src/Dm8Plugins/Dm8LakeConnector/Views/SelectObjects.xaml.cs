a74dfb5fb6eecaba65ea4d7d7363e485f82aecee
Micha Kuhlen <135815786+m0quai@users.noreply.github.com>
2025-03-27 22:47:27 +0100

    multiple changes (WIP)

--- src/Dm8Plugins/Dm8LakeConnector/Views/SelectObjects.xaml.cs
+++ src/Dm8Plugins/Dm8LakeConnector/Views/SelectObjects.xaml.cs
@@ -30,158 +30,218 @@ namespace Dm8LakeConnector.Views
 #pragma warning disable CS8618
 #pragma warning disable CS8600
 
-    public partial class SelectObjects : Window
-    {
-        public SelectObjects()
-        {
-            InitializeComponent();
-            OkButton.IsEnabled = false;
-        }
+   public partial class SelectObjects:Window
+   {
+      public SelectObjects()
+      {
+         InitializeComponent();
+         OkButton.IsEnabled = false;
+      }
 
-        public string BaseFolder { get; set; }
-        public IList<RawModelEntryBase> Entities
-        {
-            set
+      public string BaseFolder { get; set; }
+      public string LastPath
+      {
+         get
+         {
+            var storageItems = BaseFolder.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar]);
+            return (storageItems.LastOrDefault());
+
+         }
+      }
+      public string RootFolder
+      {
+         get
+         {
+            var storageItems = BaseFolder.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar]);
+            if(storageItems.Length == 1)
             {
-                _entities = value;
-                loadData();
+               return storageItems[0];
+            } else
+            {
+               string folderPath = String.Join("/", storageItems.Skip(1).ToArray());
+               return (folderPath);
             }
-        }
-
-        public RawModelEntryBase SelectedEntry
-        {
-            get
+         }
+      }
+      public string RootName
+      {
+         get
+         {
+            var storageItems = this.BaseFolder.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar]);
+            if(storageItems.Length == 1)
             {
-                RawModelEntryBase retVal = null;
-                foreach (DisplayObject<RawModelEntryBase> itm in SelectedFolderItems)
-                {
-                    retVal = itm.Cargo;
-                    break;
-                }
-                return (retVal);
+               return storageItems[0];
+            } else
+            {
+               string folderPath = String.Join("/", storageItems.SkipLast(1).ToArray());
+               return (folderPath);
             }
-        }
-        private IList<RawModelEntryBase> _entities = new List<RawModelEntryBase>();
-        private string _currentFolder = "";
-        public IList<DisplayObject<RawModelEntryBase>> SelectedFolderItems
-        {
-            get
-            {
-                IList<DisplayObject<RawModelEntryBase>> files = new List<DisplayObject<RawModelEntryBase>>();
-                foreach (var file in _allFiles)
-                {
-                    if (makeFolderName(file.Cargo.Entity.FolderName) == _currentFolder)
-                    {
-                        files.Add(file);
-                    }
-                }
-                GridObjects.ItemsSource = files;
-                return (files);
-            }
-        }
-        private IList<DisplayObject<RawModelEntryBase>> _allFiles = new List<DisplayObject<RawModelEntryBase>>();
+         }
+      }
 
-        private void CancelButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            this.DialogResult = false;
-        }
+      public IList<RawModelEntryBase> Entities
+      {
+         set
+         {
+            _entities = value;
+            loadData();
+         }
+      }
 
-        private void OkButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            this.DialogResult = true;
-        }
-        private void loadData()
-        {
-            List<DisplayFolder> folders = new List<DisplayFolder>();
-            Root.Header = this.BaseFolder;
-            Root.IsExpanded = true;
-            foreach (var item in _entities)
+      public RawModelEntryBase SelectedEntry
+      {
+         get
+         {
+            RawModelEntryBase retVal = null;
+            foreach(DisplayObject<RawModelEntryBase> itm in SelectedFolderItems)
             {
-                switch (item.Entity.ObjectType)
-                {
-                    case "Folder":
-                        DisplayFolder f = new DisplayFolder
-                        {
-                            Name = item.Entity.Name,
-                            FullName = item.Entity.FolderName
-                        };
-                        string owner = makeFolderName(f.FullName).Replace($"|{f.Name}", "");
-                        DisplayFolder parent = folders.FirstOrDefault(x => makeFolderName(x.FullName) == owner);
-                        folders.Add(f);
-                        TreeViewItem tvi = new TreeViewItem { Header = f.Name, Tag = f, IsExpanded = true };
-
-                        if (parent != null)
-                        {
-                            TreeViewItem t = findItem(Root.Items, owner);
-                            if (t != null)
-                            {
-                                t.Items.Add(tvi);
-                            }
-                            parent.Folders.Add(f);
-                        }
-                        else
-                        {
-                            Root.Items.Add(tvi);
-                        }
-                        break;
-                    case "File":
-                        if (Path.GetExtension(item.Entity.DisplayName).ToLower() == ".parquet" || Path.GetExtension(item.Entity.DisplayName).ToLower() == ".csv")
-                        {
-                            DisplayObject<RawModelEntryBase> obj = new DisplayObject<RawModelEntryBase>();
-                            obj.Name = item.Entity.Name;
-                            obj.DisplayName = item.Entity.DisplayName;
-                            obj.Schema = item.Schema;
-                            obj.Type = item.Entity.ObjectType;
-                            obj.Source = item.Function.SourceLocation;
-                            obj.Cargo = item;
-                            _allFiles.Add(obj);
-                        }
-                        break;
-                }
-            }
-        }
-        private TreeViewItem findItem(ItemCollection list ,string search)
-        {
-            TreeViewItem retVal = null;
-            foreach (TreeViewItem itm in list)
-            {
-                DisplayFolder df = (DisplayFolder)itm.Tag;
-                if (makeFolderName(df.FullName) == search)
-                {
-                    retVal = itm;
-                }
-                else
-                {
-                    if (itm.Items.Count > 0)
-                    {
-                        retVal = findItem(itm.Items, search);
-                    }
-                }
-                if (retVal != null)
-                {
-                    break;
-                }
+               retVal = itm.Cargo;
+               if(PathOnly.IsChecked == true)
+               {
+                  retVal.Function.SourceLocation = retVal.Function.SourceLocation.Replace(retVal.Entity.Name ,"*");
+               }
+               break;
             }
             return (retVal);
-        }
-        private string makeFolderName(string name)
-        {
-            name = name.Replace("/", "|");
-            name = name.Replace("\\", "|");
-            return (name);
-        }
-        private void TreeObject_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            TreeViewItem tvi = e.NewValue as TreeViewItem;
-            if (tvi != null)
+         }
+      }
+      private IList<RawModelEntryBase> _entities = new List<RawModelEntryBase>();
+      private string _currentFolder = "";
+      public IList<DisplayObject<RawModelEntryBase>> SelectedFolderItems
+      {
+         get
+         {
+            IList<DisplayObject<RawModelEntryBase>> files = new List<DisplayObject<RawModelEntryBase>>();
+            foreach(var file in _allFiles)
             {
-                DisplayFolder df = (DisplayFolder)tvi.Tag;
-                if(df != null)
-                {
-                    _currentFolder = makeFolderName(df.FullName);
-                }
+               if(makeFolderName(file.Cargo.Entity.FolderName) == _currentFolder)
+               {
+                  files.Add(file);
+               }
             }
-            OkButton.IsEnabled = this.SelectedFolderItems.Count > 0;
-        }
-    }
+            GridObjects.ItemsSource = files;
+            return (files);
+         }
+      }
+      private IList<DisplayObject<RawModelEntryBase>> _allFiles = new List<DisplayObject<RawModelEntryBase>>();
+
+      private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+      {
+         this.DialogResult = false;
+      }
+
+      private void OkButton_OnClick(object sender, RoutedEventArgs e)
+      {
+         this.DialogResult = true;
+      }
+      private void loadData()
+      {
+         List<DisplayFolder> folders = new List<DisplayFolder>();
+         Root.Header = this.RootName;
+         Root.IsExpanded = true;
+
+         DisplayFolder f = new DisplayFolder
+         {
+            Name = this.LastPath,
+            FullName = this.RootFolder
+         };
+         folders.Add(f);
+         TreeViewItem tvi = new TreeViewItem { Header = f.Name, Tag = f, IsExpanded = true };
+         Root.Items.Add(tvi);
+
+         foreach(var item in _entities)
+         {
+            switch(item.Entity.ObjectType)
+            {
+               case "Folder":
+                  f = new DisplayFolder
+                  {
+                     Name = item.Entity.Name,
+                     FullName = item.Entity.FolderName
+                  };
+                  String owner = makeFolderName(f.FullName).Replace($"|{f.Name}", "");
+                  DisplayFolder parent = folders.FirstOrDefault(x => makeFolderName(x.FullName) == owner);
+                  folders.Add(f);
+                  tvi = new TreeViewItem { Header = f.Name, Tag = f, IsExpanded = true };
+
+                  if(parent != null)
+                  {
+                     TreeViewItem t = findItem(Root.Items, owner);
+                     if(t != null)
+                     {
+                        t.Items.Add(tvi);
+                     }
+                     parent.Folders.Add(f);
+                  } else
+                  {
+                     Root.Items.Add(tvi);
+                  }
+                  break;
+               case "File":
+                  if(Path.GetExtension(item.Entity.DisplayName).ToLower() == ".parquet" || Path.GetExtension(item.Entity.DisplayName).ToLower() == ".csv")
+                  {
+                     DisplayObject<RawModelEntryBase> obj = new DisplayObject<RawModelEntryBase>();
+                     obj.Name = item.Entity.Name;
+                     obj.DisplayName = item.Entity.DisplayName;
+                     obj.Schema = item.Schema;
+                     obj.Type = item.Entity.ObjectType;
+                     obj.Source = item.Function.SourceLocation;
+                     obj.Cargo = item;
+                     _allFiles.Add(obj);
+                  }
+                  break;
+            }
+         }
+      }
+      private TreeViewItem findItem(ItemCollection list, string search)
+      {
+         TreeViewItem retVal = null;
+         foreach(TreeViewItem itm in list)
+         {
+            DisplayFolder df = (DisplayFolder)itm.Tag;
+            if(makeFolderName(df.FullName) == search)
+            {
+               retVal = itm;
+            } else
+            {
+               if(itm.Items.Count > 0)
+               {
+                  retVal = findItem(itm.Items, search);
+               }
+            }
+            if(retVal != null)
+            {
+               break;
+            }
+         }
+         return (retVal);
+      }
+      private string makeFolderName(string name)
+      {
+         name = name.Replace("/", "|");
+         name = name.Replace("\\", "|");
+         return (name);
+      }
+      private void TreeObject_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+      {
+         TreeViewItem tvi = e.NewValue as TreeViewItem;
+         if(tvi != null)
+         {
+            DisplayFolder df = (DisplayFolder)tvi.Tag;
+            if(df != null)
+            {
+               _currentFolder = makeFolderName(df.FullName);
+            } else
+            {
+               _currentFolder = "";
+            }
+         }
+         OkButton.IsEnabled = this.SelectedFolderItems.Count > 0;
+      }
+
+      private void PathOnly_Click(object sender, RoutedEventArgs e)
+      {
+
+      }
+   }
 }

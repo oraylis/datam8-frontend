9876035553f031f883d1ef884223094dbea1763e
Micha Kuhlen <135815786+m0quai@users.noreply.github.com>
2025-03-27 22:47:27 +0100

    multiple changes (WIP)

--- src/Dm8Main/Views/ProjectView.xaml.cs
+++ src/Dm8Main/Views/ProjectView.xaml.cs
@@ -9,32 +9,40 @@ using PropertyTools.Wpf;
 
 namespace Dm8Main.Views
 {
-    /// <summary>
-    /// Interaction logic for ProjectView.xaml
-    /// </summary>
-    public partial class ProjectView : UserControl, IAnchorView
-    {
-        [ImportingConstructor]
-        public ProjectView(ProjectViewModel projectViewModel)
-        {
-            this.InitializeComponent();
-            this.DataContext = projectViewModel;            
-        }
+   /// <summary>
+   /// Interaction logic for ProjectView.xaml
+   /// </summary>
+   public partial class ProjectView:UserControl, IAnchorView
+   {
+      [ImportingConstructor]
+      public ProjectView(ProjectViewModel projectViewModel)
+      {
+         this.InitializeComponent();
+         this.DataContext = projectViewModel;
+      }
 
-        public AnchorViewModel ViewModel => (ProjectViewModel)this.DataContext;
+      public AnchorViewModel ViewModel => (ProjectViewModel)this.DataContext;
 
-        private void TreeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (this.projectTree.SelectedItem is ProjectItem projectItem)
+      private void TreeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+      {
+         if(this.projectTree.SelectedItem is ProjectItem projectItem)
+         {
+            ProjectViewModel pvm = (ProjectViewModel)this.DataContext;
+            pvm.ItemSelect(projectItem);
+            this.InputBindings.Clear();
+            this.InputBindings.AddRange(projectItem.InputBindings);
+
+            TreeListBoxItem item = e.OriginalSource as TreeListBoxItem;
+            if(item != null)
             {
-                ProjectViewModel pvm = (ProjectViewModel)this.DataContext;
-                pvm.ItemSelect(projectItem);
-
-                this.InputBindings.Clear();
-                this.InputBindings.AddRange(projectItem.InputBindings);
+               item.IsSelected = true;
             }
-
-
-        }
-    }
+         }
+      }
+      public void ChangeSelection(ProjectItem projectItem)
+      {
+         this.projectTree.SelectedItem = projectItem;
+         projectItem.IsSelected = true;
+      }
+   }
 }

7a10b9e45e9dd3dc79196fb24e0658a0a7ba03f9
Fabio Kayser <f.kayser@oraylis.de>
2026-04-02 10:33:10 +0200

    Removed generator code

--- submodules/datam8-generator/Update-Submodules.ps1
+++
@@ -1,9 +0,0 @@
-write-host "Initializing and updating submodules..."
-
-try {
-    git submodule init
-    git submodule update --recursive --remote
-    write-host "Submodules initialized and updated successfully."
-} catch {
-    write-error "An error occured while Initializing or updating submoUles: $_"
-}
